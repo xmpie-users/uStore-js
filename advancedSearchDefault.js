7e95310bb84e716cf1e736ab99d71d9a1c7e739a
scouch1 <scouch1@gmail.com>
2016-09-29 08:28:49 +1000

    Create advancedSearchDefault.js

---
+++ advancedSearchDefault.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+  $(document).ready(function (){
+    if (window.location.href.toLowerCase().indexOf ("orderhistory") > -1)
+      ChangeSearchType(3);
+  })
+</script>
