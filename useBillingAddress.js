18b733d25429d8133c6630596436e324d6a5b5f6
scouch1 <scouch1@gmail.com>
2016-09-28 12:53:10 +1000

    Create useBillingAddress.js

---
+++ useBillingAddress.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+  $(document).ready(function(){
+    if (window.location.href.toLowerCase().indexOf ("checkoutfinal") > -1)
+      $('#radAsBilling').prop('checked','checked');
+  })
+</script>
