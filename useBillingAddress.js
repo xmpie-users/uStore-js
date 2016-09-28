cdf3510d58cfe19a1d515ef71dcf7704c30c3d3a
scouch1 <scouch1@gmail.com>
2016-09-28 15:35:08 +1000

    Delete useBillingAddress.js

--- useBillingAddress.js
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-  $(document).ready(function(){
-    if (window.location.href.toLowerCase().indexOf ("checkoutfinal") > -1)
-      $('#radAsBilling').prop('checked','checked');
-  })
-</script>
