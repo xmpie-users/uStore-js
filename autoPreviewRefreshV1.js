fcae9c5bf5f6c3885ebb1e90668465751d2dad61
scouch1 <scouch1@gmail.com>
2016-09-29 10:31:19 +1000

    Create autoPreviewRefreshV1.js

---
+++ autoPreviewRefreshV1.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+  $(document).ready(function(){
+    $(".Duc[dialid] [tabindex]").change(function(){
+      window.location.href=$("[nowaitingdiv]").attr("href")
+    });
+  });
+</script>
