457fa7a81457acff98d9183f49879676ecf8a90f
wb77 <wb77@tpg.com.au>
2016-09-29 10:07:34 +1000

    Add ParamTextBoxCal.js File

---
+++ ParamTextBoxCal.js
@@ -0,0 +1,7 @@
+<script src="/uStoreAdmin/Js/jquery-ui-1.10.3.custom.min.js"></script>
+    <link rel="stylesheet" href="/uStoreAdmin/Css/jquery-ui.custom.css">
+	<script>
+$( document ).ready(function() {
+    $( '#<%= tbParam.ClientID %>').datepicker({ dateFormat: "dd/mm/yy" });	
+  }); 
+</script>
