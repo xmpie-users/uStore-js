bc9b0f3e8a195c35abf501f13f0e683525cefd66
scouch1 <scouch1@gmail.com>
2016-09-29 10:43:36 +1000

    Create autoPreviewRequestV2.js

---
+++ autoPreviewRequestV2.js
@@ -0,0 +1,36 @@
+<script type="text/javascript">
+$("document").ready(function() {
+  // only if there is a link to "update thumbnail" on the page
+  if ($("#ctl00_cphMainContent_ucDialCustomization_btnUpdateThumbnails").length > 0)
+  { 
+    //set form input tags keyup event to refresh the preview after 4 second delay
+    $("input").keyup(function() {
+      wait(
+        function(){
+          if ($("#ctl00_cphMainContent_ucDialCustomization_btnUpdateThumbnails").length() > 0) {
+            document.location.href = $("#ctl00_cphMainContent_ucDialCustomization_btnUpdateThumbnails").attr("href"); 
+          }
+        },
+        4000); //4000ms = 4 seconds
+    });
+    //set form input tags onchange event to refresh the preview after 4 second delay
+    $(".Duc[dialid] [tabindex]").change(function() {
+      wait(
+        function(){
+          if ($("#ctl00_cphMainContent_ucDialCustomization_btnUpdateThumbnails").length() > 0) {
+            document.location.href = $("#ctl00_cphMainContent_ucDialCustomization_btnUpdateThumbnails").attr("href"); 
+          }
+        },
+        4000); //4000ms = 4 seconds
+    });
+  }
+});
+//this is a helper function used to delay the callback for x milliseconds.
+var wait = function(){
+  var timer = 0;
+  return function(callback, ms){
+    clearTimeout(timer);
+    timer = setTimeout(callback, ms);
+  }  
+}();
+</script>
