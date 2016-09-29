e944e13e383b4f3e504f527b2dfd257fd558229f
scouch1 <scouch1@gmail.com>
2016-09-29 14:21:44 +1000

    Update autoRecalculatePrices.js

--- autoRecalculatePrices.js
+++ autoRecalculatePrices.js
@@ -1,34 +1,36 @@
 <script type="text/javascript">
 $(document).ready(function() { 
 
-  // Auto Calculate Price ID's
-  var ddlAutoCalc = ["10809"]; // Dropdown lists
-  var txtAutoCalc = ["10810"]; // Text fields
+  // product property dial ids:
+  var ddlAutoCalc = ["10809"]; // array of Dropdown list dial ids
+  var txtAutoCalc = ["10810"]; // array of Text field dial ids
 
-	var pathname = window.location.pathname.toLowerCase(); 
-
-	if (pathname.indexOf("orderfinalstep.aspx") > 0) { 
-		// if drop down menu price steps
-		$("#ctl00_cphMainContent_ddlNumCopies").on('change', function() {
-			autoRecalculate(); 
-		});
-		// if only text input for quantity
-		$("#ctl00_cphMainContent_txtNumCopies").on('input', function() {
-			autoRecalculate(); 
-		});
-
+  var pathname = window.location.pathname.toLowerCase(); 
+  if (pathname.indexOf("orderfinalstep.aspx") > 0) { 
+    // update if dropdown quantity changes
+    $("#ctl00_cphMainContent_ddlNumCopies").on('change', function() {
+      autoRecalculate(); 
+    });
+  
+    // update if text input quantity changes
+    $("#ctl00_cphMainContent_txtNumCopies").on('input', function() {
+      autoRecalculate(); 
+    });
+    
+    //update if drop-down list product properties change
     $.each(ddlAutoCalc, function( index, value ) {
       $("#ctl00_cphMainContent_ctl11_Duc" + value + "_DropDownList").on('change', function() {
         autoRecalculate(); 
       });
     });
-              
+    
+    //update if text product properties change
     $.each(txtAutoCalc, function( index, value ) {
       $("#ctl00_cphMainContent_ctl11_Duc" + value + "_StringTextBox").on('blur', function() {
         autoRecalculate(); 
       });
     });          
-	}
+  }
 });
 
 function autoRecalculate () {
