59943aeecbdef259a85eaaf850255375d3b92197
cdalyPA <cdaly@printavenue.com>
2016-11-02 14:53:14 -0500

    Refactored autoPreviewRequestV2
    Refactored functions and variables
    Now includes code to clear the timers when a user automatically selects the Refresh Preview link

--- autoPreviewRequestV2.js
+++ autoPreviewRequestV2.js
@@ -1,36 +1,59 @@
-<script type="text/javascript">
-$("document").ready(function() {
-  // only if there is a link to "update thumbnail" on the page
-  if ($("#ctl00_cphMainContent_ucDialCustomization_btnUpdateThumbnails").length > 0)
-  { 
-    //set form input tags keyup event to refresh the preview after 4 second delay
-    $("input").keyup(function() {
-      wait(
-        function(){
-          if ($("#ctl00_cphMainContent_ucDialCustomization_btnUpdateThumbnails").length > 0) {
-            document.location.href = $("#ctl00_cphMainContent_ucDialCustomization_btnUpdateThumbnails").attr("href"); 
-          }
+var divBtnUpdateThumbnailsName = "#ctl00_cphMainContent_ucDialCustomization_btnUpdateThumbnails";
+var divBtnUpdateThumbnailsExists = false;
+var refreshInterval = 4000; //4000ms = 4 seconds
+var timeouts = []; //keep track of our timers so that we can cancel them at will
+
+function setupWait(timeInterval) {
+    wait(
+        function() {
+            if (divBtnUpdateThumbnailsExists) {
+                document.location.href = $(divBtnUpdateThumbnailsName).attr("href");
+            }
         },
-        4000); //4000ms = 4 seconds
-    });
-    //set form input tags onchange event to refresh the preview after 4 second delay
-    $(".Duc[dialid] [tabindex]").change(function() {
-      wait(
-        function(){
-          if ($("#ctl00_cphMainContent_ucDialCustomization_btnUpdateThumbnails").length > 0) {
-            document.location.href = $("#ctl00_cphMainContent_ucDialCustomization_btnUpdateThumbnails").attr("href"); 
-          }
-        },
-        4000); //4000ms = 4 seconds
-    });
-  }
-});
+        timeInterval
+	);
+}
+
 //this is a helper function used to delay the callback for x milliseconds.
-var wait = function(){
-  var timer = 0;
-  return function(callback, ms){
-    clearTimeout(timer);
-    timer = setTimeout(callback, ms);
-  }  
+var wait = function() {
+    var timer = 0;
+    return function(callback, ms) {
+        clearTimeout(timer);
+        timer = setTimeout(callback, ms);
+        timeouts.push(timer);
+    }
 }();
-</script>
+
+//function to cancel Refresh Preview timers when link is clicked
+function waitCancel() {
+    for (var i = 0; i < timeouts.length; i++) {
+        clearTimeout(timeouts[i]);
+    }
+    timeouts = []; //quick reset of the timer array
+}
+
+//reset the additional onclick method to cancel the timers
+$(document).ajaxStart(function() {
+    if (divBtnUpdateThumbnailsExists) { //make sure to cancel our onblur timers if 
+        $(divBtnUpdateThumbnailsName).on("click", waitCancel);
+    }
+});
+
+$("document").ready(function() {
+    //see if the thumbnail div exists
+    divBtnUpdateThumbnailsExists = $(divBtnUpdateThumbnailsName).length > 0;
+
+    // only if there is a link to "update thumbnail" on the page
+    if (divBtnUpdateThumbnailsExists) {
+        //set form input tags keyup event to refresh the preview after refresh interval
+        $("input").keyup(function() {
+            setupWait(refreshInterval);
+        });
+        //set form input tags onchange event to refresh the preview after refresh interval
+        $(".Duc[dialid] [tabindex]").change(function() {
+            setupWait(refreshInterval);
+        });
+    }
+});
+
+
