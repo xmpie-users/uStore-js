ae27585407ad15f7a80f165a37e213654f24eeca
V Schi <viktor@xmpie.com>
2016-12-22 13:15:40 +0100

    Create CostCenterAutoPopulation.js

---
+++ CostCenterAutoPopulation.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+if(window.location.pathname.indexOf("CheckoutPaymentSubmission.aspx") > 0){
+   $( document ).ready(function() {
+        document.getElementById("ctl00_cphMainContent_ctlClearingUserData10000_txtCostCenter").defaultValue = xmp.sdk.storeFrontParams.currentUser.custom1
+   });
+}
+</script>
